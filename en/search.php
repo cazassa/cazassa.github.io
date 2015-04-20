252c50d17121f512adaf618760d52ebdeb82e685
cazassa <educazassa@Eduardos-MacBook-Pro.local>
2015-04-20 12:14:31 -0300

    Commit Old Site

---
+++ en/search.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="search/search.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/jquery.mobilemenu.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.ui.totop.js"></script>
+    <script src="js/jquery.touchSwipe.min.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+    
+    
+    
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="assets/tm/css/tm_docs.css" type="text/css" media="screen">
+    <script src="assets/assets/js/html5shiv.js"></script>    
+    <script src="assets/assets/js/respond.min.js"></script>
+    <div style='text-align:center'><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div>  
+  <![endif]-->
+</head>
+
+<body>
+<!--==============================header=================================-->
+<header id="header" class="headbot1">
+      <div class="container">
+          <h1 class="navbar-brand navbar-brand_"><a href="index.html"><img alt="Investment advisor" src="img/logo.png"></a></h1>
+          <div class="menuheader">
+            <nav class="navbar navbar-default navbar-static-top tm_navbar" role="navigation">
+                <ul class="nav sf-menu">
+                  <li><a href="index.html">home</a></li>
+                  <li><a href="index-1.html">about us</a></li>
+                  <li><a href="index-2.html">projects</a>
+                    <ul>
+                      <li><a href="#">columns 01</a></li>
+                      <li><a href="#">columns 02</a></li>
+                      <li><a class="last" href="#">columns 03</a>
+                        <ul>
+                           <li><a href="#">columns 04</a></li>
+                           <li><a class="last" href="#">columns 05</a></li>                       
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a href="index-3.html">Our approach</a></li>
+                  <li><a href="index-4.html">contacts</a></li>
+                </ul>
+            </nav>
+          </div>
+      </div>
+</header>
+<section id="content">
+<!--==============================row15=================================-->
+<div class="row_15">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <h2 class="h2-pad">Search result:</h2>
+                <div id="search-results"></div>
+            </div>
+        </div>
+  </div>
+</div>
+</section>
+<!--==============================footer=================================-->
+<footer>
+    <div class="container">
+        <a class="smalllogo" href="index.html"><img alt="Investment advisor" src="img/smalllogo.png"></a>  
+        <div class="">
+            <p class="footerprivlink">&copy; 2013<br><a href="index-5.html"> Privacy policy</a></p>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+</body>
+</html>
